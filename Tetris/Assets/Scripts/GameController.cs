fcbefbc33e54d40df4376d6813521eb945ebacc2
Merada Richter <merada.richter@gmail.com>
2014-10-13 09:51:36 +0200

    Adds sprite hack

--- Tetris/Assets/Scripts/GameController.cs
+++ Tetris/Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 
 	public GUIText text;
+	public Sprite sprite;
 	bool create = true;
 
 	// Use this for initialization
@@ -14,7 +15,7 @@ public class GameController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (create) {
-			GridController.GetInstance().SpawnNewTetrimino();
+			GridController.GetInstance().SpawnNewTetrimino(sprite);
 			//GridController.GetInstance().SetGUIText(text);
 			create = false;
 		}
